0d7110aa90ad7276f507face9a7280216a60e3f4
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-12-15 05:03:45 -0500

    update

--- myapp/app/profile/[id]/page.js
+++ myapp/app/profile/[id]/page.js
@@ -1,22 +1,47 @@
 'use client'
-import React, { useState, useEffect } from 'react';
-import { useSession } from 'next-auth/react';
-import axios from 'axios';
+import React, { useState } from 'react';
 import BookingList from '@/components/BookingList';
+import BookingForm from '@/components/BookingForm';
+import {signIn, signOut, useSession} from 'next-auth/react'
 
-const ProfilePage = ({ userId}) => {
-  const { data: session } = useSession({ required: true });
 
+const ProfilePage = ({ userId }) => {
+  const [activeTab, setActiveTab] = useState('myBookings');
+  const { data: session } = useSession();
 
   return (
     <section className='px-4 py-12'>
       <div className='flex flex-col'>
-        <span>Hi {session?.user?.name}</span>
-        <BookingList userId={session?.user?.id} />
-      
+        <span className='text-xl font-semibold mb-4'>Hi, User</span>
+
+        <div className='flex space-x-4 mb-6'>
+          <button
+            onClick={() => setActiveTab('myBookings')}
+            className={`py-2 px-4 focus:outline-none ${
+              activeTab === 'myBookings' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-800'
+            }`}
+          >
+            My Bookings
+          </button>
+          <button
+            onClick={() => setActiveTab('newRide')}
+            className={`py-2 px-4 focus:outline-none ${
+              activeTab === 'newRide' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-800'
+            }`}
+          >
+            New Ride
+          </button>
+        </div>
+
+        {activeTab === 'myBookings' && <BookingList userId={session?.user?.id} />}
+        {activeTab === 'newRide' && <BookingForm />}
       </div>
     </section>
   );
 };
 
 export default ProfilePage;
+
+
+
+
