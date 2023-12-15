639695389098fd4dafe92375792cbcb2bf12991d
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-12-15 05:57:53 -0500

    update style

--- myapp/app/profile/[id]/page.js
+++ myapp/app/profile/[id]/page.js
@@ -2,8 +2,7 @@
 import React, { useState } from 'react';
 import BookingList from '@/components/BookingList';
 import BookingForm from '@/components/BookingForm';
-import {signIn, signOut, useSession} from 'next-auth/react'
-
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 const ProfilePage = ({ userId }) => {
   const [activeTab, setActiveTab] = useState('myBookings');
@@ -11,21 +10,21 @@ const ProfilePage = ({ userId }) => {
 
   return (
     <section className='px-4 py-12'>
-      <div className='flex flex-col'>
-        <span className='text-xl font-semibold mb-4'>Hi, User</span>
+      <div className='flex flex-col max-w-screen-xl mx-auto'>
+        <span className='text-xl font-semibold mb-4'>Hi, {session?.user.name}</span>
 
-        <div className='flex space-x-4 mb-6'>
+        <div className='flex items-center w-full justify-center'>
           <button
             onClick={() => setActiveTab('myBookings')}
-            className={`py-2 px-4 focus:outline-none ${
-              activeTab === 'myBookings' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-800'
+            className={`py-2 px-4 focus:outline-none rounded-tl-lg rounded-tr-lg w-full ${
+              activeTab === 'myBookings' ? 'bg-secondary text-white' : 'bg-gray-200 text-gray-800'
             }`}
           >
             My Bookings
           </button>
           <button
             onClick={() => setActiveTab('newRide')}
-            className={`py-2 px-4 focus:outline-none ${
+            className={`py-2 px-4 focus:outline-none rounded-tl-lg rounded-tr-lg w-full ${
               activeTab === 'newRide' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-800'
             }`}
           >
@@ -33,15 +32,13 @@ const ProfilePage = ({ userId }) => {
           </button>
         </div>
 
-        {activeTab === 'myBookings' && <BookingList userId={session?.user?.id} />}
-        {activeTab === 'newRide' && <BookingForm />}
+        <div className='bg-gray-200 border border-t-transparent border-gray-300 p-4 rounded-bl-lg rounded-br-lg'>
+          {activeTab === 'myBookings' && <BookingList userId={session?.user?.id} />}
+          {activeTab === 'newRide' && <BookingForm />}
+        </div>
       </div>
     </section>
   );
 };
 
 export default ProfilePage;
-
-
-
-
--- myapp/tailwind.config.js
+++ myapp/tailwind.config.js
@@ -7,11 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        primary: '#32c5ff',
+        secondary: '#ffa219',
+        yellow: '#9d9d9d',
+        orange: '#1a2f52',
+        mildOrange: '#f4743b'
+      }
     },
   },
   plugins: [],
